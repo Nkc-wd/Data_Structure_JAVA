15e0a1d7694da5eb3f11d13202baffafdbd44b11
niraj <nkcit001@gmail.com>
2020-06-11 18:24:28 +0530

    update search

--- Data Structure/src/searching/Binary.java
+++ Data Structure/src/searching/Binary.java
@@ -11,23 +11,26 @@ public class Binary {
 	
 
 	}
+	// return type with statement method String
 	public static String binarySearch(int[] arr, int item) {
 
-		int lo = 0;
-		int hi = arr.length - 1;
-		while (lo <= hi) {
-			int mid = (lo + hi) / 2;
+		int lo = 0;     // left
+		int hi = arr.length - 1; // right
+		while (lo <= hi) {    // loop till right is greater or equal to the left
+			int mid = (lo + hi) / 2; // mid
 			
-			if (arr[mid] < item) {
-				lo = mid + 1;
-			} else if (arr[mid] > item) {
-				hi = mid - 1;
+			if (item>arr[mid]) {  // when item is greater then mid element
+				lo = mid + 1;    // then it need to be left increment with mid , mid+1
+			} 
+			
+			else if (item<arr[mid]) {  // when item is less than mid element
+				hi = mid - 1;         // then it need to right decrement as with mid, mid-1
 			} else {
-				return ("your item has found at "+mid+" index");
-			}
+				return ("your item has found at "+mid+" index");   // When both if and else if condition pass 
+			}                                                     // then search item will be in mid, so return this
 
 		}
 
-		return ("Sorry your item is not in the list");
-	}
+		return ("Sorry your item is not in the list");    // when item is not find in while loop
+	}                                                     //then it need to be return -1
 }
--- Data Structure/src/searching/Linear.java
+++ Data Structure/src/searching/Linear.java
@@ -11,14 +11,20 @@ public class Linear {
 
 	}
 	
+	// When need to return with Statement then use String for method return
 	public static String linear_search(int[] arr,int item) {
-		for (int i = 0; i < arr.length; i++) {
-			if(arr[i]==item) {
-				return ("Item "+item+" found at "+(i+1)+" index");
+		for (int i = 0; i < arr.length; i++) {  // iterate over array
+			if(arr[i]==item) {    // When array element will be equal search item
+				return ("Item "+item+" found at "+(i+1)+" index");  // then return
 			}
 			
 		}
-		return ("not found");
+		return ("not found");  // if not then return statement
+		
+		/* Important Note:
+		 * when element is not found in iterate loop then not find return must be 
+		 * outside of the loop not with << else of if >>
+		 */
 		
 		
 	}
