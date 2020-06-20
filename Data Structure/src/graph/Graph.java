2b8b171ebaf7c1dce85ebfe7323e302c2022d5a5
niraj <nkcit001@gmail.com>
2020-06-20 12:53:05 +0530

    Graph

--- Data Structure/src/graph/Graph.java
+++ Data Structure/src/graph/Graph.java
@@ -16,7 +16,9 @@ public class Graph {
 	}
 	
 	public int numVertex() { // how many vertex in graph
+		
 		return this.vtces.size();  // size of vtces hashmap will give no of vertices
+		
 	}
 	
 	public boolean containsVertex(String vname) { //  to check that given vextex is present or not
@@ -32,6 +34,26 @@ public class Graph {
 		vtces.put(vname, vtx);  // empty hashMap puted
 		
 	}
+	
+	public void removeVertex(String vname) {
+		// we cant simply remove vertex we need to remove from nbrs of his vertex
+	     /*   1. get that all nbrs
+	          2. Then visit on nbrs and remove from there
+	          3. then remove that vertex from HM of vtces
+	     */
+		
+		Vertex vtx=vtces.get(vname);  // 1.
+		ArrayList<String> keys=new ArrayList<>(vtx.nbrs.keySet()); // List of all keys get
+		
+		for(String key:keys) {
+			Vertex nbrVtx=vtces.get(key); //2.  // get that address
+			nbrVtx.nbrs.remove(key);  // loop will go and remove from nbrs
+		}
+		
+		vtces.remove(vname); //3.
+		
+	    
+	}
 
 	
 	public int numEdges() { // to give number of edges in Graph
@@ -45,7 +67,7 @@ public class Graph {
 	  int count=0;
 	  for(String key:keys) {
 		  Vertex vtx=vtces.get(key); // vertex find
-		  count+=vtx.nbrs.size();// vtx nbrs give individual HM size and add with count
+		  count=count+vtx.nbrs.size();// vtx nbrs give individual HM size and add with count
 	  }
 	  return count/2;
 	}
@@ -66,6 +88,56 @@ public class Graph {
 		
 		
 	}
+	
+	// Add Edge bw to given vertex with edge as cost integer
+	public void addEdge(String vname1,String vname2,int cost) {
+		Vertex vtx1=vtces.get(vname1); // vertex 1 found
+		Vertex vtx2=vtces.get(vname2);  // vertex 2 found
+		
+		// let vertex's given by user not exist and second vertex is not in nbrs then must return false
+		
+		if(vtx1==null || vtx2==null || vtx1.nbrs.containsKey(vname2)) {
+				return;  // if not present then return 
+		}
+		
+		// else valid then
+		vtx1.nbrs.put(vname2, cost); //  vtx1 nbrs put vname 2 with given cost
+		vtx2.nbrs.put(vname1, cost); //  vtx2 nbrs put vname 1 with given cost
+		
+	}
+	
+	public void removeEdge(String vname1, String vname2) {
+		Vertex vtx1=vtces.get(vname1); // vertex 1 found
+		Vertex vtx2=vtces.get(vname2);  // vertex 2 found
+		
+		// let vertex's given by user not exist and second vertex is not in nbrs then must return false
+		
+		if(vtx1==null || vtx2==null || !vtx1.nbrs.containsKey(vname2)) {
+				return;  // if not present then return 
+		}
+		
+		vtx1.nbrs.remove(vname2);  // removed vtx1 nbrs of given vname2
+		vtx2.nbrs.remove(vname1); //  removed vtx2 nbrs of given vname1
+		
+		// And remember for remove from HM we need only key as vertex here value be self delete
+		
+	}
+	
+	public void display() {
+
+		System.out.println("------------------");
+		ArrayList<String> keys = new ArrayList<>(vtces.keySet()); //all keys as vertex in ARL
+
+		for (String key : keys) {
+
+			Vertex vtx = vtces.get(key);// get that key's address
+
+			System.out.println(key+" : "+vtx.nbrs);  // print that key with address's nbrs HM
+		}
+		System.out.println("------------------");
+
+	}	
+	
 }
 
 
--- Data Structure/src/graph/Graph_User.java
+++ Data Structure/src/graph/Graph_User.java
@@ -4,7 +4,26 @@ public class Graph_User {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Graph graph = new Graph();
+		graph.addVertex("A");
+		graph.addVertex("B");
+		graph.addVertex("C");
+		graph.addVertex("D");
+		graph.addVertex("E");
+		graph.addVertex("F");
+		graph.addVertex("G");
+		graph.addEdge("A", "B", 2);
+		graph.addEdge("A", "D", 5);
+		graph.addEdge("B", "C", 3);
+		graph.addEdge("C", "D", 1);
+		graph.addEdge("D", "E", 8);
+		graph.addEdge("E", "F", 5);
+		graph.addEdge("E", "G", 6);
+		graph.addEdge("F", "G", 4);
 
+		graph.display();
+		 System.out.println(graph.numVertex());
+				 System.out.println(graph.numEdges());
 	}
 
 }
